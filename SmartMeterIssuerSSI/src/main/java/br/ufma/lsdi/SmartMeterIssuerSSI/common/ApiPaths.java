6650318d6ca61dc0b7cea491f820bde007c1ba4f
Edson Carlos <edson.sampaio@lsdi.ufma.br>
2026-03-12 09:53:49 -0300

    feat(issuer): config Spring Security public access
    - Add `SecurityConfig` with SecurityFilterChain configuration.
    - Allowed public access to root page, static assets, and webhook endpoint.
    - Disabled CSRF protection specifically for Aries webhook events.
    
    Rationale: The webhook endpoint is publicly accessible and exempt from CSRF because it receives Aries Cloud Agent events, which cannot authenticate via user sessions or browser-based CSRF tokens.

--- SmartMeterIssuerSSI/src/main/java/br/ufma/lsdi/SmartMeterIssuerSSI/common/ApiPaths.java
+++ SmartMeterIssuerSSI/src/main/java/br/ufma/lsdi/SmartMeterIssuerSSI/common/ApiPaths.java
@@ -1,6 +1,7 @@
 package br.ufma.lsdi.SmartMeterIssuerSSI.common;
 
 public final class ApiPaths {
+    public static final String WEBHOOK_SECURE = "/webhook/topic/**";
     public static final String WEBHOOK = "/webhook/topic/{topic}";
 
     public static final String CREATE_INVITATION = "/create-invitation";
---
+++ SmartMeterIssuerSSI/src/main/java/br/ufma/lsdi/SmartMeterIssuerSSI/configs/SecurityConfig.java
@@ -0,0 +1,44 @@
+package br.ufma.lsdi.SmartMeterIssuerSSI.configs;
+
+import br.ufma.lsdi.SmartMeterIssuerSSI.common.ApiPaths;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf
+                        .ignoringRequestMatchers(ApiPaths.WEBHOOK_SECURE)
+                )
+                .authorizeHttpRequests((requests) ->  requests
+                        .requestMatchers(
+                                "/",
+                                "/css/**",
+                                "/js/**",
+                                ApiPaths.WEBHOOK_SECURE
+                        ).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(Customizer.withDefaults())
+                .logout(LogoutConfigurer::permitAll);
+
+        return http.build();
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
